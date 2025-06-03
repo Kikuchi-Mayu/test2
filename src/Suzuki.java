cec59ce65a50085866b9b43be03c48524a57a6d2
SS-Marugame <suzukishu@cy-com.co.jp>
2025-06-03 15:30:22 +0900

    Scanner

--- src/Suzuki.java
+++ src/Suzuki.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Suzuki {
-  
+  public static void main(String[] args) {
+    String name;
+    try (Scanner scan = new Scanner(System.in)) {
+      name = scan.nextLine();
+    }
+    System.out.printf("こんにちは%sさん", name);
+  }
 }
